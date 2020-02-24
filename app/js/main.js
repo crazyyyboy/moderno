2ea497b8b4d26efc8cab387734ac482f54ec6024
crazyyyboy <jen1ka228@gmail.com>
2020-02-24 14:00:42 +0300

    last section

--- app/js/main.js
+++ app/js/main.js
@@ -50,6 +50,8 @@ $(function(){
          $('#'+id).addClass('active-tab').fadeIn();
          return false;
       });
+
+      $('input[type="file"], select').styler();
    
 
    var mixer = mixitup('.products__inner-box'); 
